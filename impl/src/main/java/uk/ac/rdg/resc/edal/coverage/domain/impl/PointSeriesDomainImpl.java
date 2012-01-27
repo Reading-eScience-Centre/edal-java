2694e16be0864328969a5a46c0de51deaddb2431
griffiths.guy <devnull@localhost>
2012-01-27 17:01:30 +0000

    Added some doc, formatted to Java standard

--- impl/src/main/java/uk/ac/rdg/resc/edal/coverage/domain/impl/PointSeriesDomainImpl.java
+++ impl/src/main/java/uk/ac/rdg/resc/edal/coverage/domain/impl/PointSeriesDomainImpl.java
@@ -9,26 +9,40 @@ import uk.ac.rdg.resc.edal.position.CalendarSystem;
 import uk.ac.rdg.resc.edal.position.TimePosition;
 import uk.ac.rdg.resc.edal.util.Extents;
 
+/**
+ * This is a {@link List} backed implementation of a {@link PointSeriesDomain}
+ * 
+ * @author Guy Griffiths
+ * 
+ */
 public class PointSeriesDomainImpl implements PointSeriesDomain {
 
     private final List<TimePosition> times;
     private final CalendarSystem calSys;
-    
+
+    /**
+     * Instantiates a new {@link PointSeriesDomain} from a {@link List} of
+     * {@link TimePosition}s
+     * 
+     * @param times
+     *            the {@link List} of {@link TimePosition}s
+     */
     public PointSeriesDomainImpl(List<TimePosition> times) {
         this.times = times;
         long lastTime = 0L;
-        if(times.size() == 0){
-            throw new IllegalArgumentException("Must have at least one time value for a PointSeriesDomain");
+        if (times.size() == 0) {
+            throw new IllegalArgumentException(
+                    "Must have at least one time value for a PointSeriesDomain");
         }
-        for(TimePosition time : times){
-            if(time.getValue() < lastTime){
+        for (TimePosition time : times) {
+            if (time.getValue() < lastTime) {
                 throw new IllegalArgumentException("List of times must be in ascending order");
             }
             lastTime = time.getValue();
         }
         calSys = times.get(0).getCalendarSystem();
     }
-    
+
     @Override
     public CalendarSystem getCalendarSystem() {
         return calSys;
@@ -41,29 +55,30 @@ public class PointSeriesDomainImpl implements PointSeriesDomain {
 
     @Override
     public Extent<TimePosition> getExtent() {
-        return Extents.newExtent(times.get(0), times.get(times.size()-1));
+        return Extents.newExtent(times.get(0), times.get(times.size() - 1));
     }
 
     @Override
     public boolean contains(TimePosition position) {
-        return (position.getValue() >= times.get(0).getValue() && position.getValue() <= times.get(times.size()-1).getValue());
+        return (position.getValue() >= times.get(0).getValue() && position.getValue() <= times.get(
+                times.size() - 1).getValue());
     }
 
     @Override
     public long findIndexOf(TimePosition time) {
         int index = Collections.binarySearch(times, time);
-        if(index >= 0){
+        if (index >= 0) {
             return index;
         } else {
-            int insertionPoint = -(index+1);
-            if(insertionPoint == times.size() || insertionPoint == 0){
+            int insertionPoint = -(index + 1);
+            if (insertionPoint == times.size() || insertionPoint == 0) {
                 return -1;
             }
-            if(Math.abs(times.get(insertionPoint).getValue() - time.getValue()) < 
-               Math.abs(times.get(insertionPoint-1).getValue() - time.getValue())){
+            if (Math.abs(times.get(insertionPoint).getValue() - time.getValue()) < Math.abs(times
+                    .get(insertionPoint - 1).getValue() - time.getValue())) {
                 return insertionPoint;
             } else {
-                return insertionPoint-1;
+                return insertionPoint - 1;
             }
         }
     }
