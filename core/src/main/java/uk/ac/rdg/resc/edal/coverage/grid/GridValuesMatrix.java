afd7852cb5dec59bdc650dcab12948fa9f3f016a
Jon Blower <j.d.blower@reading.ac.uk>
2012-05-08 22:29:11 +0100

    Simplified GridValuesMatrix (it's not clear that we need those extra methods)

--- core/src/main/java/uk/ac/rdg/resc/edal/coverage/grid/GridValuesMatrix.java
+++ core/src/main/java/uk/ac/rdg/resc/edal/coverage/grid/GridValuesMatrix.java
@@ -4,7 +4,6 @@
  */
 package uk.ac.rdg.resc.edal.coverage.grid;
 
-import java.util.List;
 import uk.ac.rdg.resc.edal.util.BigList;
 
 /**
@@ -15,12 +14,6 @@ import uk.ac.rdg.resc.edal.util.BigList;
 public interface GridValuesMatrix<E> extends Grid {
     
     public BigList<E> getValues();
-    
-    public E getValue(int i, int j);
-    
-    public List<E> getScanline(int j, int imin, int imax);
-    
-    public GridValuesMatrix<E> getGrid(int imin, int imax, int jmin, int jmax);
     
     public Class<E> getValueType();
     
