f654ff26c4a989a9345c3d157c1e2a1c84cc965f
griffiths.guy <devnull@localhost>
2012-07-10 15:23:40 +0100

    Moved formats to sub-package Restructured graphics package - MapPlotter does higher level organisation, Frame does the actual rendering

---
+++ graphics/src/main/java/uk/ac/rdg/resc/edal/graphics/PlotStyle.java
@@ -0,0 +1,5 @@
+package uk.ac.rdg.resc.edal.graphics;
+
+public enum PlotStyle {
+    BOXFILL, VECTOR, POINT, TRAJECTORY, CONTOUR, DEFAULT
+}
--- graphics/src/main/java/uk/ac/rdg/resc/edal/graphics/formats/PngFormat.java
+++ graphics/src/main/java/uk/ac/rdg/resc/edal/graphics/formats/PngFormat.java
@@ -25,7 +25,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *******************************************************************************/
-package uk.ac.rdg.resc.edal.graphics;
+package uk.ac.rdg.resc.edal.graphics.formats;
 
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
