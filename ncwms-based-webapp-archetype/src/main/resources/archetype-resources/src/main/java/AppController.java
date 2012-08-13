0abbf87206b59bf29b19a9d3547a94c32e41c707
griffiths.guy <devnull@localhost>
2012-08-13 17:29:43 +0100

    Removed unused repo Updated documentation Improved archtype a little to show a concrete example of using the Config object

--- ncwms-based-webapp-archetype/src/main/resources/archetype-resources/src/main/java/AppController.java
+++ ncwms-based-webapp-archetype/src/main/resources/archetype-resources/src/main/java/AppController.java
@@ -3,6 +3,8 @@ package $groupId;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
@@ -11,7 +13,9 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 
+import uk.ac.rdg.resc.edal.feature.Feature;
 import uk.ac.rdg.resc.ncwms.config.Config;
+import uk.ac.rdg.resc.ncwms.config.Dataset;
 
 public class AppController extends MultiActionController {
     
@@ -36,7 +40,18 @@ public class AppController extends MultiActionController {
             throws IOException {
         ServletOutputStream outputStream = response.getOutputStream();
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream));
-        writer.write("AppController is working correctly");
+        writer.write("AppController is working correctly.  We have the data:\n");
+        Map<String, Dataset> allDatasets = config.getAllDatasets();
+        for(Entry<String, Dataset> dataset : allDatasets.entrySet()){
+            writer.write(dataset.getKey()+":\n");
+            for(Feature feature : dataset.getValue().getFeatureCollection()){
+                writer.write("\t"+feature.getName()+":\n");
+                for(String memberName : feature.getCoverage().getScalarMemberNames()){
+                    writer.write("\t\t"+memberName+"\n");
+                }
+            }
+        }
+        
         writer.close();
         outputStream.close();
         return null;
