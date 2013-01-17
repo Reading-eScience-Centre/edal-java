28259cc25e7b6bb7b6ac11b30ce62637cd19515b
charlesjohnroberts <charlesjohnroberts@gmail.com>
2013-01-17 17:02:30 +0000

    Initial implementation of 1D case of colour scheme (ColourScheme1D) and associated files.

--- graphics/src/main/java/uk/ac/rdg/resc/edal/graphics/style/RasterPlotter.java
+++ graphics/src/main/java/uk/ac/rdg/resc/edal/graphics/style/RasterPlotter.java
@@ -4,6 +4,9 @@ import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.util.List;
 
+import uk.ac.rdg.resc.edal.Extent;
+import uk.ac.rdg.resc.edal.util.Extents;
+
 public class RasterPlotter extends Plotter {
     
     private ColourScheme colourScheme;
@@ -14,30 +17,19 @@ public class RasterPlotter extends Plotter {
          * extract one data value per pixel.
          */
         super(1, PlotType.RASTER);
-        colourScheme = new ColourScheme(1) {
-            @Override
-            public Color doGetColor(Number... values) {
-                if(Float.isNaN(values[0].floatValue()))
-                    return Color.blue;
-                float intensity = (values[0].floatValue() - 273)/40f;
-                if(intensity > 1.0)
-                    intensity = 1.0f;
-                if(intensity < 0.0)
-                    intensity = 0.0f;
-                return new Color(intensity, 0f, 0f);
-            }
-        };
     }
 
     @Override
     protected void drawIntoImage(BufferedImage image, List<PlottingDatum>[] data) {
-        /*
+    	Extent<Float> scaleRange = Extents.newExtent(270.0f, 310.0f);
+    	colourScheme = new ColourScheme1D(scaleRange, null, null, Color.black, "redblue", 100, 254, false);
+    	/*
          * We can directly access data[0], because the data array is guaranteed
          * to be of size 1 (because it is specified in the constructor, and
          * checks occur in the superclass)
          */
         for(PlottingDatum datum : data[0]){
-            image.setRGB(datum.getGridCoords().getXIndex(), datum.getGridCoords().getYIndex(),
+        	image.setRGB(datum.getGridCoords().getXIndex(), datum.getGridCoords().getYIndex(),
                     colourScheme.doGetColor(datum.getValue()).getRGB());
         }
     }
