0df7d86efa7d026b826aa41db06f8ae44476581b
Guy Griffiths <guy.griffiths@rdg.ac.uk>
2013-01-29 14:54:21 +0000

    Put in correct xml namespace

--- graphics/src/main/java/uk/ac/rdg/resc/edal/graphics/style/datamodel/model/FlatOpacity.java
+++ graphics/src/main/java/uk/ac/rdg/resc/edal/graphics/style/datamodel/model/FlatOpacity.java
@@ -1,7 +1,9 @@
 package uk.ac.rdg.resc.edal.graphics.style.datamodel.model;
 
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
 
+@XmlType(namespace=ImageData.NAMESPACE)
 public class FlatOpacity extends OpacityTransform {
     @XmlElement(name="Opacity")
     private Float opacity = 1.0f;
--- graphics/src/main/java/uk/ac/rdg/resc/edal/graphics/style/datamodel/model/OpacityTransform.java
+++ graphics/src/main/java/uk/ac/rdg/resc/edal/graphics/style/datamodel/model/OpacityTransform.java
@@ -1,5 +1,8 @@
 package uk.ac.rdg.resc.edal.graphics.style.datamodel.model;
 
+import javax.xml.bind.annotation.XmlType;
+
+@XmlType(namespace=ImageData.NAMESPACE)
 public abstract class OpacityTransform {
     public abstract Float getOpacityForValue(Float value);
 }
