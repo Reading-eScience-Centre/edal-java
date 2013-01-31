6e230cce2ce61793efbf149ce763379a505cd1d4
Guy Griffiths <guy.griffiths@rdg.ac.uk>
2013-01-31 17:04:33 +0000

    Removed styling experiment stuff.  This is now in a new branch (style-experiment) and can be merged back later when it's all decided on

--- graphics/src/main/java/uk/ac/rdg/resc/edal/graphics/style/datamodel/model/OpacityTransform.java
+++
@@ -1,8 +0,0 @@
-package uk.ac.rdg.resc.edal.graphics.style.datamodel.model;
-
-import javax.xml.bind.annotation.XmlType;
-
-@XmlType(namespace=ImageData.NAMESPACE, name="OpacityTransformType")
-public abstract class OpacityTransform {
-    public abstract Float getOpacityForValue(Float value);
-}
