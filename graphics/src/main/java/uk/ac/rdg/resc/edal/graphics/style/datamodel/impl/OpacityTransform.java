fdd9adf340b6835ffe25f2450bb18bc283609dc5
Guy Griffiths <guy.griffiths@rdg.ac.uk>
2013-02-08 13:42:01 +0000

    Combined data and plotting classes - separation was unnecessary

--- graphics/src/main/java/uk/ac/rdg/resc/edal/graphics/style/datamodel/impl/OpacityTransform.java
+++ graphics/src/main/java/uk/ac/rdg/resc/edal/graphics/style/datamodel/impl/OpacityTransform.java
@@ -1,8 +1,9 @@
-package uk.ac.rdg.resc.edal.graphics.style.datamodel.model;
+package uk.ac.rdg.resc.edal.graphics.style.datamodel.impl;
 
 import javax.xml.bind.annotation.XmlType;
 
-@XmlType(namespace=ImageData.NAMESPACE, name="OpacityTransformType")
+
+@XmlType(namespace=Image.NAMESPACE, name="OpacityTransformType")
 public abstract class OpacityTransform {
     public abstract Float getOpacityForValue(Float value);
 }
---
+++ graphics/src/main/java/uk/ac/rdg/resc/edal/graphics/style/datamodel/impl/RasterLayer.java
@@ -0,0 +1,50 @@
+package uk.ac.rdg.resc.edal.graphics.style.datamodel.impl;
+
+import java.awt.image.BufferedImage;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+import uk.ac.rdg.resc.edal.graphics.style.DataReadingTypes.PlotType;
+import uk.ac.rdg.resc.edal.graphics.style.PlottingDatum;
+
+@XmlType(namespace = Image.NAMESPACE, name = "RasterLayerType")
+public class RasterLayer extends ImageLayer {
+    
+    @XmlElement(name = "DataFieldName", required = true)
+    private String dataFieldName;
+    @XmlElement(name = "ColourScheme")
+    private ColourScheme1DData colourScheme = new ColourScheme1DData();
+    
+    @SuppressWarnings("unused")
+    private RasterLayer() {
+        super(PlotType.RASTER);
+    }
+    
+    public RasterLayer(String dataFieldName, ColourScheme1DData colourScheme) {
+        super(PlotType.RASTER);
+        this.dataFieldName = dataFieldName;
+        this.colourScheme = colourScheme;
+    }
+
+    public String getDataFieldName() {
+        return dataFieldName;
+    }
+
+    public ColourScheme1DData getColourScheme() {
+        return colourScheme;
+    }
+
+    @Override
+    protected void drawIntoImage(BufferedImage image, DataReader dataReader) {
+        /*
+         * We can directly access data[0], because the data array is guaranteed
+         * to be of size 1 (because it is specified in the constructor, and
+         * checks occur in the superclass)
+         */
+        for (PlottingDatum datum : dataReader.getDataForLayerName(dataFieldName)) {
+            image.setRGB(datum.getGridCoords().getXIndex(), datum.getGridCoords().getYIndex(),
+                    colourScheme.getColor(datum.getValue()).getRGB());
+        }
+    }
+}
