d2d92014686cd210b6a6d3b2e5148f1683025826
charlesjohnroberts <charlesjohnroberts@gmail.com>
2013-03-04 16:11:35 +0000

    Fixed XML annotations for SimpleGlyphLayer class.

--- graphics/src/main/java/uk/ac/rdg/resc/edal/graphics/style/datamodel/impl/GlyphLayer.java
+++ graphics/src/main/java/uk/ac/rdg/resc/edal/graphics/style/datamodel/impl/GlyphLayer.java
@@ -2,8 +2,11 @@ package uk.ac.rdg.resc.edal.graphics.style.datamodel.impl;
 
 import java.awt.image.BufferedImage;
 
+import javax.xml.bind.annotation.XmlType;
+
 import uk.ac.rdg.resc.edal.graphics.style.DataReadingTypes.PlotType;
 
+@XmlType(namespace = Image.NAMESPACE, name = "GlyphLayerType")
 public abstract class GlyphLayer extends ImageLayer {
 
 	public GlyphLayer() {
