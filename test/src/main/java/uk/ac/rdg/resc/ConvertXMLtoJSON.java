31cfa6b91b3866a089367d627ba557b288525503
charlesjohnroberts <charlesjohnroberts@gmail.com>
2013-05-20 15:06:29 +0100

    Initial attempt at a JSON parser for ncWMS. It uses the apache maven library json-libs to parse a JSON document to an XML string that is interpretable by JAXB.

---
+++ test/src/main/java/uk/ac/rdg/resc/ConvertXMLtoJSON.java
@@ -0,0 +1,24 @@
+package uk.ac.rdg.resc;
+
+import java.io.InputStream;
+
+import net.sf.json.JSON;
+import net.sf.json.xml.XMLSerializer;
+
+import org.apache.commons.io.IOUtils;
+
+public class ConvertXMLtoJSON {
+
+	public static void main(String[] args) throws Exception {
+
+		InputStream is = ConvertJSONtoXML.class.getResourceAsStream("/xml/blackening.xml");
+		String xmlData = IOUtils.toString(is);
+		
+		XMLSerializer serializer = new XMLSerializer();
+		JSON json = serializer.read(xmlData);
+		
+		System.out.println(json.toString(2));
+		
+	}
+
+}
