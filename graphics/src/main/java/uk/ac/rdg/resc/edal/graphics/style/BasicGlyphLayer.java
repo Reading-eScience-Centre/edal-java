8cc67d4557d3305e0b4564d2d474b2cbb5f96630
griffiths guy <griffiths.guy@code.google.com>
2013-08-09 15:46:16 +0100

    First copy over of old graphics stuff.  Not fully compiling yet (currently all based on FeatureCollections...)

---
+++ graphics/src/main/java/uk/ac/rdg/resc/edal/graphics/style/BasicGlyphLayer.java
@@ -0,0 +1,31 @@
+package uk.ac.rdg.resc.edal.graphics.style;
+
+import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.annotation.XmlType;
+
+import uk.ac.rdg.resc.edal.graphics.style.util.DataReadingTypes.PlotType;
+
+@XmlType(namespace = Image.NAMESPACE, name = "BasicGlyphLayerType")
+public class BasicGlyphLayer extends GlyphLayer {
+    /*
+     * This gets called after being unmarshalled from XML. This reads in the
+     * icon for the glyph.
+     */
+    void afterUnmarshal( Unmarshaller u, Object parent ) {
+    	icon = getIcon(this.glyphIconName);
+    }
+
+   public BasicGlyphLayer() throws InstantiationException {
+		super(PlotType.GLYPH);
+	}
+	
+	public BasicGlyphLayer(String dataFieldName, String glyphIconName,
+			ColourScheme colourScheme) throws InstantiationException {
+		super(PlotType.GLYPH);
+		
+		this.dataFieldName = dataFieldName;
+		this.glyphIconName = glyphIconName;
+		this.colourScheme = colourScheme;	
+	}
+
+}
---
+++ graphics/src/main/java/uk/ac/rdg/resc/edal/graphics/style/ThresholdColourScheme.java
@@ -0,0 +1,106 @@
+package uk.ac.rdg.resc.edal.graphics.style;
+
+import java.awt.Color;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+import uk.ac.rdg.resc.edal.graphics.style.util.StyleXMLParser.ColorAdapter;
+
+@XmlType(namespace = Image.NAMESPACE, propOrder = {"thresholds", "colours", "noDataColour"}, name = "ThresholdColourSchemeType")
+public class ThresholdColourScheme extends ColourScheme {
+
+    /*
+     * These hold lists of colours and the value which marks the lower boundary
+     * of their threshold, IN REVERSE ORDER OF VALUES. It is supplied in
+     * ascending order (because this is the logical way to think of things), but
+     * is reversed in the setter.
+     */
+    @XmlElement(name = "Thresholds", required = true)
+    private List<Float> thresholds;
+    @XmlElement(name = "Colours", required = true)
+    @XmlJavaTypeAdapter(ColorAdapter.class)
+    private List<Color> colours;
+    
+    @XmlElement(name = "MissingDataColour")
+    @XmlJavaTypeAdapter(ColorAdapter.class)
+    private Color noDataColour = new Color(0f, 0f, 0f, 0f);
+    
+    /*
+     * This gets called after being unmarshalled from XML. This initialises the
+     * colours correctly
+     */
+    void afterUnmarshal( Unmarshaller u, Object parent ) {
+        initialiseColours();
+    }
+
+    ThresholdColourScheme() {}
+
+    public ThresholdColourScheme(List<Float> thresholds, List<Color> colours, Color noDataColour) {
+        super();
+        this.noDataColour = noDataColour;
+        this.thresholds = thresholds;
+        this.colours = colours;
+        initialiseColours();
+    }
+
+    @Override
+    public Color getColor(Number value) {
+        if(value == null || Float.isNaN(value.floatValue())) {
+            return noDataColour;
+        }
+        /*
+         * Remember: THIS LIST IS IN REVERSE ORDER.
+         */
+        Iterator<Color> colourIterator = colours.iterator();
+		Color colour = colourIterator.next();
+        for(Float band : thresholds) {
+            if(value.floatValue() > band) {
+                return colour;
+            }
+            colour = colourIterator.next();
+        }
+        return colour;
+    }
+    
+    private void initialiseColours() {
+        if(thresholds == null || thresholds.size() < 1) {
+            throw new IllegalArgumentException("Threshold values must not be null and must have at least one value");
+        }
+        /*
+         * Check that there are the correct number of colours.
+         */
+        if (colours == null || colours.size() != (thresholds.size() + 1)) {
+        	throw new IllegalArgumentException("Colours must not be null and must be in the correct number to match the thresholds.");
+        }
+        /*
+         * Check that thresholds are correctly ordered.  Then reverse lists. 
+         */
+        Float value = -Float.MAX_VALUE;
+        for(Float band : thresholds) {
+            if(band < value) {
+                throw new IllegalArgumentException(
+                        "Threshold bands must be in ascending order of value");
+            }
+            value = band;
+        }
+        Collections.reverse(thresholds);
+        
+        Collections.reverse(colours);
+    }
+
+    @Override
+    public Float getScaleMin() {
+        return thresholds.get(0);
+    }
+
+    @Override
+    public Float getScaleMax() {
+        return thresholds.get(thresholds.size() - 1);
+    }
+}
