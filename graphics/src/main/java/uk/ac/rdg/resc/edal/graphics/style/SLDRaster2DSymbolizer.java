a83f0e044fb806433234c979665d1c28385e36ee
charlesjohnroberts <charlesjohnroberts@gmail.com>
2013-09-19 14:24:18 +0100

    Stylistic improvements to SLD parser.

--- graphics/src/main/java/uk/ac/rdg/resc/edal/graphics/style/SLDRaster2DSymbolizer.java
+++ graphics/src/main/java/uk/ac/rdg/resc/edal/graphics/style/SLDRaster2DSymbolizer.java
@@ -64,51 +64,44 @@ public class SLDRaster2DSymbolizer implements SLDSymbolizer {
 		
 		// get name of x data field
 		Node xDataFieldNameNode = (Node) xPath.evaluate(
-				"./se:Geometry/resc:XDataFieldName",
-				symbolizerNode, XPathConstants.NODE);
+				"./se:Geometry/resc:XDataFieldName", symbolizerNode, XPathConstants.NODE);
 		if (xDataFieldNameNode == null) {
-			return null;
+			throw new SLDException("The x data field name must be specified in a geometry tag.");
 		}
 		String xDataFieldName = xDataFieldNameNode.getTextContent();
 		if (xDataFieldName.equals("")) {
-			return null;
+			throw new SLDException("The x data field name cannot be empty.");
 		}
 		
 		// get name of y data field
 		Node yDataFieldNameNode = (Node) xPath.evaluate(
-				"./se:Geometry/resc:YDataFieldName",
-				symbolizerNode, XPathConstants.NODE);
+				"./se:Geometry/resc:YDataFieldName", symbolizerNode, XPathConstants.NODE);
 		if (yDataFieldNameNode == null) {
-			return null;
+			throw new SLDException("The y data field name must be specified in a geometry tag.");
 		}
 		String yDataFieldName = yDataFieldNameNode.getTextContent();
 		if (yDataFieldName.equals("")) {
-			return null;
+			throw new SLDException("The y data field name cannot be empty.");
 		}
 		
 		// get opacity element if it exists
 		Node opacityNode = (Node) xPath.evaluate(
-				"./se:Opacity",
-				symbolizerNode, XPathConstants.NODE);
-		String opacity;
+				"./se:Opacity", symbolizerNode, XPathConstants.NODE);
+		String opacity = null;
 		if (opacityNode != null) {
 			opacity = opacityNode.getTextContent();
-		} else {
-			opacity = "";
 		}
-		
+
 		// get the function defining the colour map
 		Node function = (Node) xPath.evaluate(
-				"./se:ColorMap/*",
-				symbolizerNode, XPathConstants.NODE);
+				"./resc:ColorMap2D/*", symbolizerNode, XPathConstants.NODE);
 		if (function == null || function.getNodeType() != Node.ELEMENT_NODE) {
-			return null;
+			throw new SLDException("The raster symbolizer must contain a function.");
 		}
 		
 		// get fall back value
 		String fallbackValue = (String) xPath.evaluate(
-				"./@fallbackValue",
-				function, XPathConstants.STRING);
+				"./@fallbackValue", function, XPathConstants.STRING);
 		Color noDataColour;
 		if (fallbackValue == null) {
 			noDataColour = null;
@@ -118,60 +111,67 @@ public class SLDRaster2DSymbolizer implements SLDSymbolizer {
 		
 		// parse function specific parts of XML for colour scheme
 		ColourScheme2D colourScheme2D;
-		if (function.getLocalName().equals("Categorize")) {
-			// get list of colours
-			NodeList colourNodes = (NodeList) xPath.evaluate(
-					"./se:Value",
-					function, XPathConstants.NODESET);
-			if (colourNodes == null) {
-				return null;
-			}
-			// transform to list of Color objects
-			ArrayList<Color> colours = new ArrayList<Color>();
-			for (int j = 0; j < colourNodes.getLength(); j++) {
-				Node colourNode = colourNodes.item(j);
-				colours.add(decodeColour(colourNode.getTextContent()));
-			}
-			
-			//get list of x thresholds
-			NodeList xThresholdNodes = (NodeList) xPath.evaluate(
-					"./resc:XThreshold",
-						function, XPathConstants.NODESET);
-			if (xThresholdNodes == null) {
-				return null;
-			}
-			// transform to list of Floats
-			ArrayList<Float> xThresholds = new ArrayList<Float>();
-			for (int j = 0; j < xThresholdNodes.getLength(); j++) {
-				Node thresholdNode = xThresholdNodes.item(j);
-				xThresholds.add(Float.parseFloat(thresholdNode.getTextContent()));
-			}
-			
-			//get list of y thresholds
-			NodeList yThresholdNodes = (NodeList) xPath.evaluate(
-					"./resc:YThreshold",
-						function, XPathConstants.NODESET);
-			if (yThresholdNodes == null) {
-				return null;
-			}
-			// transform to list of Floats
-			ArrayList<Float> yThresholds = new ArrayList<Float>();
-			for (int j = 0; j < yThresholdNodes.getLength(); j++) {
-				Node thresholdNode = yThresholdNodes.item(j);
-				yThresholds.add(Float.parseFloat(thresholdNode.getTextContent()));
-			}
-			
-			colourScheme2D = new ThresholdColourScheme2D(xThresholds, yThresholds, colours, noDataColour);
+		if (function.getLocalName().equals("Categorize2D")) {
+			colourScheme2D = parseCategorize2D(xPath, function, noDataColour);
 		} else {
-			return null;
+			throw new SLDException("The function must be Categorize2D.");
 		}
 		
 		// instantiate a new raster layer and add it to the image
 		Raster2DLayer raster2DLayer = new Raster2DLayer(xDataFieldName, yDataFieldName, colourScheme2D);
-		if (!opacity.equals("")) {
+		if (!(opacity == null)) {
 			raster2DLayer.setOpacityTransform(new FlatOpacity(Float.parseFloat(opacity)));
 		}
 		return raster2DLayer;
+	}
+
+	private ColourScheme2D parseCategorize2D(XPath xPath, Node function,
+			Color noDataColour) throws XPathExpressionException, SLDException {
+		ColourScheme2D colourScheme2D;
+
+		// get list of colours
+		NodeList colourNodes = (NodeList) xPath.evaluate(
+				"./se:Value", function, XPathConstants.NODESET);
+		if (colourNodes == null) {
+			throw new SLDException("The 2D categorize function must contain a list of values.");
+		}
+		
+		// transform to list of Color objects
+		ArrayList<Color> colours = new ArrayList<Color>();
+		for (int j = 0; j < colourNodes.getLength(); j++) {
+			Node colourNode = colourNodes.item(j);
+			colours.add(decodeColour(colourNode.getTextContent()));
+		}
+		
+		//get list of x thresholds
+		NodeList xThresholdNodes = (NodeList) xPath.evaluate(
+				"./resc:XThreshold", function, XPathConstants.NODESET);
+		if (xThresholdNodes == null) {
+			throw new SLDException("The 2D categorize function must contain a list of x thresholds.");
+		}
+
+		// transform to list of Floats
+		ArrayList<Float> xThresholds = new ArrayList<Float>();
+		for (int j = 0; j < xThresholdNodes.getLength(); j++) {
+			Node thresholdNode = xThresholdNodes.item(j);
+			xThresholds.add(Float.parseFloat(thresholdNode.getTextContent()));
+		}
+		
+		//get list of y thresholds
+		NodeList yThresholdNodes = (NodeList) xPath.evaluate(
+				"./resc:YThreshold", function, XPathConstants.NODESET);
+		if (yThresholdNodes == null) {
+			throw new SLDException("The 2D categorize function must contain a list of y thresholds.");
+		}
+		// transform to list of Floats
+		ArrayList<Float> yThresholds = new ArrayList<Float>();
+		for (int j = 0; j < yThresholdNodes.getLength(); j++) {
+			Node thresholdNode = yThresholdNodes.item(j);
+			yThresholds.add(Float.parseFloat(thresholdNode.getTextContent()));
+		}
+		
+		colourScheme2D = new ThresholdColourScheme2D(xThresholds, yThresholds, colours, noDataColour);
+		return colourScheme2D;
 	}
 
 }
--- graphics/src/main/java/uk/ac/rdg/resc/edal/graphics/style/SLDRasterSymbolizer.java
+++ graphics/src/main/java/uk/ac/rdg/resc/edal/graphics/style/SLDRasterSymbolizer.java
@@ -57,7 +57,7 @@ public class SLDRasterSymbolizer implements SLDSymbolizer {
 	 * Parse symbolizer using XPath
 	 */
 	private ImageLayer parseSymbolizer() throws XPathExpressionException, NumberFormatException, SLDException {
-		// make sure layer is not null an element node
+		// make sure layer is not null and an element node
 		if (symbolizerNode == null || symbolizerNode.getNodeType() != Node.ELEMENT_NODE) {
 			throw new SLDException("The symbolizer node cannot be null and must be an element node.");
 		}
@@ -67,27 +67,22 @@ public class SLDRasterSymbolizer implements SLDSymbolizer {
 		
 		// get opacity element if it exists
 		Node opacityNode = (Node) xPath.evaluate(
-				"./se:Opacity",
-				symbolizerNode, XPathConstants.NODE);
-		String opacity;
+				"./se:Opacity", symbolizerNode, XPathConstants.NODE);
+		String opacity = null;
 		if (opacityNode != null) {
 			opacity = opacityNode.getTextContent();
-		} else {
-			opacity = "";
 		}
 		
 		// get the function defining the colour map
 		Node function = (Node) xPath.evaluate(
-				"./se:ColorMap/*",
-				symbolizerNode, XPathConstants.NODE);
+				"./se:ColorMap/*", symbolizerNode, XPathConstants.NODE);
 		if (function == null || function.getNodeType() != Node.ELEMENT_NODE) {
-			return null;
+			throw new SLDException("The raster symbolizer must contain a function.");
 		}
 		
 		// get fall back value
 		String fallbackValue = (String) xPath.evaluate(
-				"./@fallbackValue",
-				function, XPathConstants.STRING);
+				"./@fallbackValue", function, XPathConstants.STRING);
 		Color noDataColour;
 		if (fallbackValue == null) {
 			noDataColour = null;
@@ -98,134 +93,147 @@ public class SLDRasterSymbolizer implements SLDSymbolizer {
 		// parse function specific parts of XML for colour scheme
 		ColourScheme colourScheme;
 		if (function.getLocalName().equals("Categorize")) {
-			// get list of colours
-			NodeList colourNodes = (NodeList) xPath.evaluate(
-					"./se:Value",
-					function, XPathConstants.NODESET);
-			if (colourNodes == null) {
-				return null;
-			}
-			// transform to list of Color objects
-			ArrayList<Color> colours = new ArrayList<Color>();
-			for (int j = 0; j < colourNodes.getLength(); j++) {
-				Node colourNode = colourNodes.item(j);
-				colours.add(decodeColour(colourNode.getTextContent()));
-			}
-			
-			//get list of thresholds
-			NodeList thresholdNodes = (NodeList) xPath.evaluate(
-					"./se:Threshold",
-						function, XPathConstants.NODESET);
-			if (thresholdNodes == null) {
-				return null;
-			}
-			// transform to list of Floats
-			ArrayList<Float> thresholds = new ArrayList<Float>();
-			for (int j = 0; j < thresholdNodes.getLength(); j++) {
-				Node thresholdNode = thresholdNodes.item(j);
-				thresholds.add(Float.parseFloat(thresholdNode.getTextContent()));
-			}
-			
-			colourScheme = new ThresholdColourScheme(thresholds, colours, noDataColour);
+			colourScheme = parseCategorize(xPath, function, noDataColour);
 		} else if (function.getLocalName().equals("Interpolate")) {
-			// get list of data points
-			NodeList pointNodes = (NodeList) xPath.evaluate(
-					"./se:InterpolationPoint",
-						function, XPathConstants.NODESET);
-			if (pointNodes == null) {
-				return null;
-			}
-			// parse into an ArrayList
-			ArrayList<InterpolateColourScheme.InterpolationPoint> points =
-					new ArrayList<InterpolateColourScheme.InterpolationPoint>();
-			for (int j = 0; j < pointNodes.getLength(); j++) {
-				Node pointNode = pointNodes.item(j);
-				Node dataNode = (Node) xPath.evaluate(
-						"./se:Data",
-						pointNode, XPathConstants.NODE);
-				if (dataNode == null) {
-					return null;
-				}
-				Node valueNode = (Node) xPath.evaluate(
-						"./se:Value",
-						pointNode, XPathConstants.NODE);
-				if (valueNode == null) {
-					return null;
-				}
-				points.add(new InterpolateColourScheme.InterpolationPoint(
-						Float.parseFloat(dataNode.getTextContent()),
-						decodeColour(valueNode.getTextContent())));
-			}
-			if (points.size() < 1) {
-				return null;
-			}
-			// create a new InterpolateColourScheme object
-			colourScheme = new InterpolateColourScheme(points, noDataColour);			
+			colourScheme = parseInterpolate(xPath, function, noDataColour);			
 		} else if (function.getLocalName().equals("Palette")) {
-			// Create the colour map
-			String paletteDefinition = (String) xPath.evaluate(
-					"./resc:PaletteDefinition",
-					function, XPathConstants.STRING);
-			if (paletteDefinition == null || paletteDefinition.equals("")) {
-				return null;
-			}
-			String nColourBandsText = (String) xPath.evaluate(
-					"./resc:NumberOfColorBands",
-					function, XPathConstants.STRING);
-			Integer nColourBands;
-			if (nColourBandsText == null || nColourBandsText.equals("")) {
-				nColourBands = 254;
-			} else {
-				nColourBands = Integer.parseInt(nColourBandsText);
-			}
-			String belowMinColourText = (String) xPath.evaluate(
-					"./resc:BelowMinColor",
-					function, XPathConstants.STRING);
-			Color belowMinColour = decodeColour(belowMinColourText);
-			String aboveMaxColourText = (String) xPath.evaluate(
-					"./resc:AboveMaxColor",
-					function, XPathConstants.STRING);
-			Color aboveMaxColour = decodeColour(aboveMaxColourText);
-			ColourMap colourMap = new ColourMap(belowMinColour, aboveMaxColour, noDataColour, paletteDefinition, nColourBands);
-			
-			// Create the colour scale
-			String scaleMinText = (String) xPath.evaluate(
-					"./resc:ColorScale/resc:ScaleMin",
-					function, XPathConstants.STRING);
-			if (scaleMinText == null || scaleMinText.equals("")) {
-				return null;
-			}
-			Float scaleMin = Float.parseFloat(scaleMinText);
-			String scaleMaxText = (String) xPath.evaluate(
-					"./resc:ColorScale/resc:ScaleMax",
-					function, XPathConstants.STRING);
-			if (scaleMaxText == null || scaleMaxText.equals("")) {
-				return null;
-			}
-			Float scaleMax = Float.parseFloat(scaleMaxText);
-			String logarithmicText = (String) xPath.evaluate(
-					"./resc:ColorScale/resc:Logarithmic",
-					function, XPathConstants.STRING);
-			Boolean logarithmic;
-			if (logarithmicText == null || logarithmicText.equals("")) {
-				logarithmic = false;
-			} else {
-				logarithmic = Boolean.parseBoolean(logarithmicText);
-			}
-			ColourScale colourScale = new ColourScale(scaleMin, scaleMax, logarithmic);
-			
-			// Create the colour scheme
-			colourScheme = new PaletteColourScheme(colourScale, colourMap);
+			colourScheme = parsePalette(xPath, function, noDataColour);
 		} else {
-			return null;
+			throw new SLDException("The function must be one of Categorize, Interpolate or Palette.");
 		}
 		
 		// instantiate a new raster layer and add it to the image
 		RasterLayer rasterLayer = new RasterLayer(layerName, colourScheme);
-		if (!opacity.equals("")) {
+		if (!(opacity == null)) {
 			rasterLayer.setOpacityTransform(new FlatOpacity(Float.parseFloat(opacity)));
 		}
 		return rasterLayer;
+	}
+
+	private ColourScheme parseCategorize(XPath xPath, Node function,
+			Color noDataColour) throws XPathExpressionException, SLDException {
+		ColourScheme colourScheme;
+
+		// get list of colours
+		NodeList colourNodes = (NodeList) xPath.evaluate(
+				"./se:Value", function, XPathConstants.NODESET);
+		if (colourNodes == null) {
+			throw new SLDException("The categorize function must contain a list ov values.");
+		}
+		
+		// transform to list of Color objects
+		ArrayList<Color> colours = new ArrayList<Color>();
+		for (int j = 0; j < colourNodes.getLength(); j++) {
+			Node colourNode = colourNodes.item(j);
+			colours.add(decodeColour(colourNode.getTextContent()));
+		}
+		
+		//get list of thresholds
+		NodeList thresholdNodes = (NodeList) xPath.evaluate(
+				"./se:Threshold", function, XPathConstants.NODESET);
+		if (thresholdNodes == null) {
+			throw new SLDException("The categorize function must contain a list of thresholds.");
+		}
+		
+		// transform to list of Floats
+		ArrayList<Float> thresholds = new ArrayList<Float>();
+		for (int j = 0; j < thresholdNodes.getLength(); j++) {
+			Node thresholdNode = thresholdNodes.item(j);
+			thresholds.add(Float.parseFloat(thresholdNode.getTextContent()));
+		}
+		
+		colourScheme = new ThresholdColourScheme(thresholds, colours, noDataColour);
+		return colourScheme;
+	}
+
+	private ColourScheme parseInterpolate(XPath xPath, Node function,
+			Color noDataColour) throws XPathExpressionException, SLDException {
+		ColourScheme colourScheme;
+
+		// get list of data points
+		NodeList pointNodes = (NodeList) xPath.evaluate(
+				"./se:InterpolationPoint", function, XPathConstants.NODESET);
+		if (pointNodes == null) {
+			throw new SLDException("The interpolate function must contain a list of interpolation points.");
+		}
+		
+		// parse into an ArrayList
+		ArrayList<InterpolateColourScheme.InterpolationPoint> points =
+				new ArrayList<InterpolateColourScheme.InterpolationPoint>();
+		for (int j = 0; j < pointNodes.getLength(); j++) {
+			Node pointNode = pointNodes.item(j);
+			Node dataNode = (Node) xPath.evaluate(
+					"./se:Data", pointNode, XPathConstants.NODE);
+			if (dataNode == null) {
+				throw new SLDException("Each interpolation point must contain a data element.");
+			}
+			Node valueNode = (Node) xPath.evaluate(
+					"./se:Value", pointNode, XPathConstants.NODE);
+			if (valueNode == null) {
+				throw new SLDException("Each interpolation point must contain a value element.");
+			}
+			points.add(new InterpolateColourScheme.InterpolationPoint(
+					Float.parseFloat(dataNode.getTextContent()),
+					decodeColour(valueNode.getTextContent())));
+		}
+
+		// create a new InterpolateColourScheme object
+		colourScheme = new InterpolateColourScheme(points, noDataColour);
+		return colourScheme;
+	}
+
+	private ColourScheme parsePalette(XPath xPath, Node function,
+			Color noDataColour) throws XPathExpressionException, SLDException {
+		ColourScheme colourScheme;
+
+		// Create the colour map
+		String paletteDefinition = (String) xPath.evaluate(
+				"./resc:PaletteDefinition", function, XPathConstants.STRING);
+		if (paletteDefinition == null || paletteDefinition.equals("")) {
+			throw new SLDException("The palette function must contain a palette definition.");
+		}
+		String nColourBandsText = (String) xPath.evaluate(
+				"./resc:NumberOfColorBands", function, XPathConstants.STRING);
+		Integer nColourBands;
+		if (nColourBandsText == null || nColourBandsText.equals("")) {
+			nColourBands = 254;
+		} else {
+			nColourBands = Integer.parseInt(nColourBandsText);
+		}
+		String belowMinColourText = (String) xPath.evaluate(
+				"./resc:BelowMinColor", function, XPathConstants.STRING);
+		Color belowMinColour = decodeColour(belowMinColourText);
+		String aboveMaxColourText = (String) xPath.evaluate(
+				"./resc:AboveMaxColor", function, XPathConstants.STRING);
+		Color aboveMaxColour = decodeColour(aboveMaxColourText);
+		ColourMap colourMap = new ColourMap(belowMinColour, aboveMaxColour, noDataColour, paletteDefinition, nColourBands);
+		
+		// Create the colour scale
+		String scaleMinText = (String) xPath.evaluate(
+				"./resc:ColorScale/resc:ScaleMin", function, XPathConstants.STRING);
+		if (scaleMinText == null || scaleMinText.equals("")) {
+			throw new SLDException("The scale minimum must be specified in a colour scale.");
+		}
+		Float scaleMin = Float.parseFloat(scaleMinText);
+		String scaleMaxText = (String) xPath.evaluate(
+				"./resc:ColorScale/resc:ScaleMax", function, XPathConstants.STRING);
+		if (scaleMaxText == null || scaleMaxText.equals("")) {
+			throw new SLDException("The scale maximum must be specified in a colour scale.");
+		}
+		Float scaleMax = Float.parseFloat(scaleMaxText);
+		String logarithmicText = (String) xPath.evaluate(
+				"./resc:ColorScale/resc:Logarithmic", function, XPathConstants.STRING);
+		Boolean logarithmic;
+		if (logarithmicText == null || logarithmicText.equals("")) {
+			logarithmic = false;
+		} else {
+			logarithmic = Boolean.parseBoolean(logarithmicText);
+		}
+		ColourScale colourScale = new ColourScale(scaleMin, scaleMax, logarithmic);
+		
+		// Create the colour scheme
+		colourScheme = new PaletteColourScheme(colourScale, colourMap);
+		return colourScheme;
 	}
 
 }
