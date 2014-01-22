eab7242f40bf065ca11b776b8dc9ec32e39a8ddc
n lin <n.lin@reading.ac.uk>
2014-01-22 11:42:57 +0000

    IN_PROGRESS_ENH - # 28: Add JUnit tests for important common classes http://www.resc.rdg.ac.uk/trac/edal/ticket/28 GetTimeRangeForString method seems have problem. Maybe because of Extents class

--- common/src/test/java/uk/ac/rdg/resc/edal/util/TimeUtilsTest.java
+++ common/src/test/java/uk/ac/rdg/resc/edal/util/TimeUtilsTest.java
@@ -1,49 +1,132 @@
 package uk.ac.rdg.resc.edal.util;
 
 import static org.junit.Assert.*;
+
 import org.joda.time.*;
+
 import uk.ac.rdg.resc.edal.util.TimeUtils;
+import uk.ac.rdg.resc.edal.util.chronologies.AllLeapChronology;
+import uk.ac.rdg.resc.edal.exceptions.BadTimeFormatException;
 import java.util.*;
+
 import org.junit.Before;
 import org.junit.Test;
 
 public class TimeUtilsTest {
-	
+	private DateTime start =new DateTime(2000,1,1,0,0, DateTimeZone.UTC);
 	private List<DateTime> datetimes = new ArrayList<DateTime>();
-	private static long MILLIS_PER_SECOND = 1000L;
-    private static long MILLIS_PER_MINUTE = 60L * MILLIS_PER_SECOND;
-    private static long MILLIS_PER_HOUR = 60L * MILLIS_PER_MINUTE;
-    private static long MILLIS_PER_DAY = 24L * MILLIS_PER_HOUR;
-    private long period;
+	private final static long MILLIS_PER_SECOND = 1000L;
+    private final static long MILLIS_PER_MINUTE = 60L * MILLIS_PER_SECOND;
+    private final static long MILLIS_PER_HOUR = 60L * MILLIS_PER_MINUTE;
+    private final static long MILLIS_PER_DAY = 24L * MILLIS_PER_HOUR;
+
     
 	@Before
-	public void setUp() throws Exception {
-		DateTime start =new DateTime(2000,1,1,0,0);
+	public void setUp() throws Exception {		
 		for(int i=0; i<10; i++)
 			datetimes.add(start.plusDays(i));
-		period = 1L *MILLIS_PER_DAY;
 	}
 
 	@Test
 	public void testgetPeriodString(){
-		long second_period = 44 *MILLIS_PER_SECOND;
-		assertEquals("PT44S", TimeUtils.getPeriodString(second_period) );
-		long minute_period = 15*MILLIS_PER_MINUTE +second_period;
-		assertEquals("PT15M44S", TimeUtils.getPeriodString(minute_period) );
-		long hour_period =20* MILLIS_PER_HOUR +minute_period;
-		assertEquals("PT20H15M44S", TimeUtils.getPeriodString(hour_period) );
-		long day_period = 3 *MILLIS_PER_DAY+ hour_period;
-		assertEquals("P3DT20H15M44S", TimeUtils.getPeriodString(day_period) );
+		long secondPeriod = 44 *MILLIS_PER_SECOND;
+		assertEquals("PT44S", TimeUtils.getPeriodString(secondPeriod) );
+		long minutePeriod = 15*MILLIS_PER_MINUTE +secondPeriod;
+		assertEquals("PT15M44S", TimeUtils.getPeriodString(minutePeriod) );
+		long hourPeriod =20* MILLIS_PER_HOUR +minutePeriod;
+		assertEquals("PT20H15M44S", TimeUtils.getPeriodString(hourPeriod) );
+		long dayPeriod = 3 *MILLIS_PER_DAY+ hourPeriod;
+		assertEquals("P3DT20H15M44S", TimeUtils.getPeriodString(dayPeriod) );
 	}
 	
 	@Test
-	public void test() {
-		String date_period = TimeUtils.getPeriodString(period);
-		int datetime_number =datetimes.size();
-		String expected =TimeUtils.dateTimeToISO8601(datetimes.get(0)) +"/"+TimeUtils.dateTimeToISO8601(datetimes.get(datetime_number-1))
-				+"/"+ date_period;
-		assertEquals(expected, TimeUtils.getTimeStringForCapabilities(datetimes) );
+	public void testgetTimeStringForCapabilities() {
+		long period = 1L * MILLIS_PER_DAY;
+		String oneDayPeriod = TimeUtils.getPeriodString(period);
+		DateTime lastDate =datetimes.get(datetimes.size()-1);
+		String expected =TimeUtils.dateTimeToISO8601(start) +"/"
+						+TimeUtils.dateTimeToISO8601(lastDate)
+						+"/" +oneDayPeriod;
+		assertEquals(expected, TimeUtils.getTimeStringForCapabilities(datetimes));
 		
+		datetimes.add(lastDate.plusDays(2));
+		lastDate =datetimes.get(datetimes.size()-1);
+		expected =expected +","+ TimeUtils.dateTimeToISO8601(lastDate);
+		assertEquals(expected, TimeUtils.getTimeStringForCapabilities(datetimes));
+		
+		datetimes.add(lastDate.plusDays(3));
+		lastDate =datetimes.get(datetimes.size()-1);
+		expected =expected +","+ TimeUtils.dateTimeToISO8601(lastDate);
+		assertEquals(expected, TimeUtils.getTimeStringForCapabilities(datetimes));
+		
+		List<DateTime> secondPeriod =new ArrayList<DateTime>();
+		for(int i=1; i<5; i++)
+			secondPeriod.add(start.plusDays(20 *i));
+		datetimes.addAll(secondPeriod);
+		String twentyDayPeriod = TimeUtils.getPeriodString(20L *period);
+		expected = expected + ","+
+		           TimeUtils.dateTimeToISO8601(secondPeriod.get(0)) +"/" +
+				   TimeUtils.dateTimeToISO8601(secondPeriod.get(secondPeriod.size()-1))+
+				   "/" +twentyDayPeriod;
+		assertEquals(expected, TimeUtils.getTimeStringForCapabilities(datetimes));
+		datetimes.add(start.plusDays(180));
+		expected = expected + ","+
+				   TimeUtils.dateTimeToISO8601(datetimes.get(datetimes.size()-1));
+		assertEquals(expected, TimeUtils.getTimeStringForCapabilities(datetimes));
+		
+		List<DateTime> emptyList =new ArrayList<DateTime>();
+		assertEquals("", TimeUtils.getTimeStringForCapabilities(emptyList));
+	}
+	
+	@Test
+	public void testOnSameDay(){
+		DateTime dt1;
+		dt1 = new DateTime(2013, 2, 27, 14,34, DateTimeZone.UTC);
+		DateTime dt2;
+		dt2 =new DateTime(2013, 2, 28, 3,34, DateTimeZone.forOffsetHours(4));
+		assertTrue(TimeUtils.onSameDay(dt1, dt2));
+		
+		dt2 =new DateTime(2013, 2, 27, 9,34, DateTimeZone.forOffsetHours(4));
+		assertTrue(TimeUtils.onSameDay(dt1, dt2));
+		
+		dt2 =new DateTime(2013, 2, 27, 23,34, DateTimeZone.forOffsetHours(-2));
+		assertFalse(TimeUtils.onSameDay(dt1, dt2));
+		
+		dt1 =new DateTime(2013, 2, 28, 14, 34);
+		dt2 =new DateTime(2013, 2, 28, 10, 34);
+		assertTrue(TimeUtils.onSameDay(dt1, dt2));
+		
+		dt1 =new DateTime(2013, 2, 25, 14,34);
+		dt2 =new DateTime(2013, 2, 28, 10,34);
+		assertFalse(TimeUtils.onSameDay(dt1, dt2));
+	}
+	
+	@Test
+	public void testFindTimeIndex(){
+		DateTime dt =new DateTime(1999,12,1,0,0, DateTimeZone.UTC);
+		int index =TimeUtils.findTimeIndex(datetimes, dt);
+		assertEquals(index, -1);
+		
+		dt =new DateTime(2000,1, 4,10,0, DateTimeZone.UTC);
+		index =TimeUtils.findTimeIndex(datetimes, dt);
+		assertEquals(index, -5);
+		
+		dt =new DateTime(2000,1,19,10,0, DateTimeZone.UTC);
+		index =TimeUtils.findTimeIndex(datetimes, dt);
+		assertEquals(index, -11);
+		
+		dt =new DateTime(2000,1, 4,0, 0, DateTimeZone.UTC);
+		index =TimeUtils.findTimeIndex(datetimes, dt);
+		assertEquals(index, 3);
+	}
+	
+	@Test
+	public void testGetTimeRangeForString(){
+		String timeString ="1990/12/30;2000/01/01";
+		try{
+			assertEquals("", TimeUtils.getTimeRangeForString(timeString, 
+                                                           AllLeapChronology.getInstanceUTC()));
+		}catch(BadTimeFormatException e){}
 	}
 
 }
