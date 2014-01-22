f7e9cddcb4643209ebf802951887a7b0eed087c3
n lin <n.lin@reading.ac.uk>
2014-01-22 11:54:40 +0000

    IN_PROGRESS_ENH - # 28: Add JUnit tests for important common classes http://www.resc.rdg.ac.uk/trac/edal/ticket/28 double d=100 is ok. but for 2d array and the type of data is Double, I can't set value in this way.

---
+++ common/src/test/java/uk/ac/rdg/resc/edal/util/ValuesArray2DTest.java
@@ -0,0 +1,23 @@
+package uk.ac.rdg.resc.edal.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ValuesArray2DTest {
+
+	@Test
+	public void test() {
+		ValuesArray2D data = new ValuesArray2D(3, 4);
+		data.set(12.0f, 2, 3);
+		assertEquals(data.get(2,3), new Float(12));
+		data.set(12L, 1, 3);
+		assertEquals(data.get(1,3), new Long(12));
+		data.set(5, 2,2);
+		assertEquals(data.get(2, 2), new Integer(5));
+		data.set(100, 1, 2);
+		assertEquals(data.get(1, 2), new Double(100.0));
+	}
+	
+
+}
