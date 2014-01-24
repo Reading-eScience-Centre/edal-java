80b740c84ea479852a6665d454e804ba05b2fcbf
n lin <n.lin@reading.ac.uk>
2014-01-24 13:46:51 +0000

    IN_PROGRESS_ENH - # 28: Add JUnit tests for important common classes http://www.resc.rdg.ac.uk/trac/edal/ticket/28 New tests.

---
+++ common/src/test/java/uk/ac/rdg/resc/edal/util/ImmutableArrayTest.java
@@ -0,0 +1,29 @@
+package uk.ac.rdg.resc.edal.util;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import org.geotoolkit.referencing.crs.DefaultGeographicCRS;
+import uk.ac.rdg.resc.edal.position.*;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+
+public class ImmutableArrayTest {
+	private final int SIZE=5;
+	
+	@Test
+	public void test() {
+		CoordinateReferenceSystem crs= DefaultGeographicCRS.WGS84;		
+		HorizontalPosition [] hPosArray =new HorizontalPosition [SIZE];
+		for(int i=0; i<SIZE; i++){
+			HorizontalPosition hPos = new HorizontalPosition(100.0 +i*0.2, 120.0+i*0.3, crs);
+			hPosArray [i] =hPos;
+		}
+		ImmutableArray1D<HorizontalPosition> hArray = new ImmutableArray1D<HorizontalPosition>(10, hPosArray);
+		assertEquals(hArray.get(3), hPosArray[3]);
+		
+		Integer [] data ={1, 2, 3, 4, 5,};
+		ImmutableArray1D<Integer> iArray = new ImmutableArray1D<Integer>(10, data);
+		assertEquals(iArray.get(10), data[10]);
+	}
+
+}
