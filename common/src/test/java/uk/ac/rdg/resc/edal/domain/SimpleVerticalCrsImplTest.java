d1ec444d0bc5c1554538832655159ba1526a25a8
n lin <n.lin@reading.ac.uk>
2014-02-07 11:20:21 +0000

    IN_PROGRESS_ENH - # 28: Add JUnit tests for important common classes http://www.resc.rdg.ac.uk/trac/edal/ticket/28

--- common/src/test/java/uk/ac/rdg/resc/edal/domain/SimpleVerticalCrsImplTest.java
+++ common/src/test/java/uk/ac/rdg/resc/edal/domain/SimpleVerticalCrsImplTest.java
@@ -7,23 +7,23 @@ import org.junit.Before;
 import uk.ac.rdg.resc.edal.position.VerticalCrsImpl;
 
 public class SimpleVerticalCrsImplTest {
-	private SimpleVerticalDomain svd;
-	
-	@Before
-	public void setUp()
-	{
-		VerticalCrsImpl height= new VerticalCrsImpl("meter", false, false, true);
-		svd =new SimpleVerticalDomain(100.0, 1000.0, height);
-	}
-	
-	@Test
-	public void testContain() {
-		assertTrue(svd.contains(500.0));
-		assertTrue(svd.contains(100.0));
-		assertTrue(svd.contains(1000.0));
-		assertFalse(svd.contains(-500.0));
-		assertFalse(svd.contains(1500.0));
-		assertFalse(svd.contains(Double.NaN));
-	}
+    private SimpleVerticalDomain svd;
+
+    @Before
+    public void setUp() {
+        VerticalCrsImpl height = new VerticalCrsImpl("meter", false, false, true);
+        svd = new SimpleVerticalDomain(100.0, 1000.0, height);
+    }
+
+    @Test
+    public void testContain() {
+        assertTrue(svd.contains(500.0));
+        assertTrue(svd.contains(100.0));
+        assertTrue(svd.contains(1000.0));
+        assertFalse(svd.contains(-500.0));
+        assertFalse(svd.contains(1500.0));
+        assertFalse(svd.contains(Double.NaN));
+        assertFalse(svd.contains(null));
+    }
 
 }
