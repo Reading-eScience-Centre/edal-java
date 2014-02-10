1dfb8c5bb39e7e5da80a06a335bbc817317e022e
n lin <n.lin@reading.ac.uk>
2014-02-10 10:30:37 +0000

    commit ready for merge.

--- common/src/test/java/uk/ac/rdg/resc/edal/grid/TimeAxisTest.java
+++ common/src/test/java/uk/ac/rdg/resc/edal/grid/TimeAxisTest.java
@@ -7,42 +7,43 @@ import org.junit.Test;
 
 import java.util.*;
 
-import org.joda.time.*; 
+import org.joda.time.*;
 import org.joda.time.chrono.ISOChronology;
 
 import uk.ac.rdg.resc.edal.domain.*;
 import uk.ac.rdg.resc.edal.util.Extents;
 
 public class TimeAxisTest {
-	private TimeAxis tAxis;
-	private Chronology chronology;
-	
-	@Before
-	public void setUp() throws Exception {
-		chronology =ISOChronology.getInstance();
-		DateTime start =new DateTime(1999, 12,1,0,0, chronology);
-		List<DateTime> datetimes =new ArrayList<DateTime>();
-		for (int i=0; i<11; i++){
-			DateTime dt = start.plusDays(2*i);
-			datetimes.add(dt);
-		}
-		tAxis =new TimeAxisImpl("Sample TimeAxis", datetimes);
-	}
+    private TimeAxis tAxis;
+    private Chronology chronology;
 
-	@Test
-	public void test() {
-		Extent<DateTime> dateBound =tAxis.getExtent();
-		DateTime firstDate =new DateTime(1999, 12,1,0,0, chronology);;
-		DateTime lastDate =firstDate.plusDays(20);
-		Extent<DateTime> expected =Extents.newExtent(firstDate, lastDate);
-		assertEquals(dateBound, expected);
-		
-		int n=4;
-		DateTime fifthDate = tAxis.getCoordinateValue(n);
-		assertEquals(tAxis.findIndexOf(fifthDate), n);
-		
-		assertTrue(tAxis.isAscending());
-		
-	}
+    @Before
+    public void setUp() throws Exception {
+        chronology = ISOChronology.getInstance();
+        DateTime start = new DateTime(1999, 12, 1, 0, 0, chronology);
+        List<DateTime> datetimes = new ArrayList<DateTime>();
+        for (int i = 0; i < 11; i++) {
+            DateTime dt = start.plusDays(2 * i);
+            datetimes.add(dt);
+        }
+        tAxis = new TimeAxisImpl("Sample TimeAxis", datetimes);
+    }
+
+    @Test
+    public void test() {
+        Extent<DateTime> dateBound = tAxis.getExtent();
+        DateTime firstDate = new DateTime(1999, 12, 1, 0, 0, chronology);
+
+        DateTime lastDate = firstDate.plusDays(20);
+        Extent<DateTime> expected = Extents.newExtent(firstDate, lastDate);
+        assertEquals(dateBound, expected);
+
+        int n = 4;
+        DateTime fifthDate = tAxis.getCoordinateValue(n);
+        assertEquals(tAxis.findIndexOf(fifthDate), n);
+
+        assertTrue(tAxis.isAscending());
+
+    }
 
 }
--- common/src/test/java/uk/ac/rdg/resc/edal/util/ColletionUtilsTest.java
+++ common/src/test/java/uk/ac/rdg/resc/edal/util/ColletionUtilsTest.java
@@ -11,56 +11,58 @@ import org.junit.Test;
 import uk.ac.rdg.resc.edal.util.CollectionUtils;
 
 public class ColletionUtilsTest {
-	private static int SIZE =100;
-	private float [] floatData =new float[SIZE];
-	private Float[] floatDataAsObject =new Float[SIZE];
-	private double [] doubleData =new double [SIZE];
-	private Double [] doubleDataAsObject =new Double [SIZE];
-	private Set<Double> doubleDataSet =new HashSet<Double>();
-	private Set<Float> floatDataSet =new HashSet<Float>();
-	
-	@Before
-	public void setUp() throws Exception {
-		for (int i=0; i<SIZE; i++){
-			floatData [i] =1.0f *i;
-			doubleData [i] =1.0 *i;
-			floatDataAsObject [i] =1.0f *i;
-			doubleDataAsObject [i] =1.0 *i;			
-			floatDataSet.add(1.0f *i);
-			doubleDataSet.add(1.0 *i);
-			
-		}
-	}
+    private static int SIZE = 100;
+    private float[] floatData = new float[SIZE];
+    private Float[] floatDataAsObject = new Float[SIZE];
+    private double[] doubleData = new double[SIZE];
+    private Double[] doubleDataAsObject = new Double[SIZE];
+    private Set<Double> doubleDataSet = new HashSet<Double>();
+    private Set<Float> floatDataSet = new HashSet<Float>();
 
-	@Test
-	public void testListFromFloatArray() {
-		List<Float> floatDataAsList =CollectionUtils.listFromFloatArray(floatData);		
-		assertEquals(floatData.length, floatDataAsList.size());		
-		for(int i=0; i<floatData.length; i++){
-			assertEquals(floatDataAsList.get(i), floatData[i], 1e-6);
-		}
-	}
-	
-	@Test
-	public void testListFromDoubleArray() {
-		List<Double> doubleDataAsList =CollectionUtils.listFromDoubleArray(doubleData);
-		assertEquals(doubleData.length, doubleDataAsList.size());
-		for(int i=0; i<doubleData.length; i++){
-			assertEquals(doubleDataAsList.get(i), doubleData [i], 1e-6);
-		}
-	}
-	
-	@Test
-	public void testSetOf() {
-		Set<Float> expectedFloatSet =CollectionUtils.setOf(floatDataAsObject);
-		Set<Double> expectedDoubleSet =CollectionUtils.setOf(doubleDataAsObject);
-		assertEquals(expectedFloatSet, floatDataSet);
-		assertEquals(expectedDoubleSet, doubleDataSet);
-	}
-	
-	@Test(expected =NullPointerException.class)
-	public void testNull(){
-		CollectionUtils.listFromFloatArray(null);
-		CollectionUtils.listFromDoubleArray(null);
-	}
+    @Before
+    public void setUp() throws Exception {
+        for (int i = 0; i < SIZE - 10; i++) {
+            floatData[i] = 1.0f * i + 1.0f;
+            doubleData[i] = 1.0 * i + 1.0;
+            floatDataAsObject[i] = 1.0f * i + 1.0f;
+            doubleDataAsObject[i] = 1.0 * i + 1.0;
+            floatDataSet.add(1.0f * i + 1.0f);
+            doubleDataSet.add(1.0 * i + 1.0);
+
+        }
+        floatDataSet.add(null);
+        doubleDataSet.add(null);
+    }
+
+    @Test
+    public void testListFromFloatArray() {
+        List<Float> floatDataAsList = CollectionUtils.listFromFloatArray(floatData);
+        assertEquals(floatData.length, floatDataAsList.size());
+        for (int i = 0; i < floatData.length; i++) {
+            assertEquals(floatDataAsList.get(i), floatData[i], 1e-6);
+        }
+    }
+
+    @Test
+    public void testListFromDoubleArray() {
+        List<Double> doubleDataAsList = CollectionUtils.listFromDoubleArray(doubleData);
+        assertEquals(doubleData.length, doubleDataAsList.size());
+        for (int i = 0; i < doubleData.length; i++) {
+            assertEquals(doubleDataAsList.get(i), new Double(doubleData[i]));
+        }
+    }
+
+    @Test
+    public void testSetOf() {
+        Set<Float> expectedFloatSet = CollectionUtils.setOf(floatDataAsObject);
+        Set<Double> expectedDoubleSet = CollectionUtils.setOf(doubleDataAsObject);
+        assertEquals(expectedFloatSet, floatDataSet);
+        assertEquals(expectedDoubleSet, doubleDataSet);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testNull() {
+        CollectionUtils.listFromFloatArray(null);
+        CollectionUtils.listFromDoubleArray(null);
+    }
 }
