aaa4c56874c20d049412230ddd3febd90a4c5344
Guy G <guy.griffiths@rdg.ac.uk>
2015-10-22 15:09:05 +0100

    Preliminary implementation of WaterML dataset factory

--- cdm/src/main/java/uk/ac/rdg/resc/edal/dataset/cdm/WaterMLDatasetFactory.java
+++ cdm/src/main/java/uk/ac/rdg/resc/edal/dataset/cdm/WaterMLDatasetFactory.java
@@ -35,7 +35,6 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Set;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -60,8 +59,9 @@ import uk.ac.rdg.resc.edal.dataset.Dataset;
 import uk.ac.rdg.resc.edal.dataset.DatasetFactory;
 import uk.ac.rdg.resc.edal.dataset.DiscreteFeatureReader;
 import uk.ac.rdg.resc.edal.dataset.FeatureIndexer;
+import uk.ac.rdg.resc.edal.dataset.FeatureIndexer.FeatureBounds;
 import uk.ac.rdg.resc.edal.dataset.PRTreeFeatureIndexer;
-import uk.ac.rdg.resc.edal.domain.Extent;
+import uk.ac.rdg.resc.edal.domain.HorizontalDomain;
 import uk.ac.rdg.resc.edal.domain.SimpleHorizontalDomain;
 import uk.ac.rdg.resc.edal.domain.TemporalDomain;
 import uk.ac.rdg.resc.edal.exceptions.DataReadingException;
@@ -69,17 +69,12 @@ import uk.ac.rdg.resc.edal.exceptions.EdalException;
 import uk.ac.rdg.resc.edal.feature.DiscreteFeature;
 import uk.ac.rdg.resc.edal.feature.PointFeature;
 import uk.ac.rdg.resc.edal.feature.PointSeriesFeature;
-import uk.ac.rdg.resc.edal.feature.ProfileFeature;
-import uk.ac.rdg.resc.edal.geometry.BoundingBox;
-import uk.ac.rdg.resc.edal.geometry.BoundingBoxImpl;
-import uk.ac.rdg.resc.edal.grid.TimeAxis;
 import uk.ac.rdg.resc.edal.grid.TimeAxisImpl;
 import uk.ac.rdg.resc.edal.metadata.Parameter;
 import uk.ac.rdg.resc.edal.metadata.VariableMetadata;
-import uk.ac.rdg.resc.edal.position.GeoPosition;
 import uk.ac.rdg.resc.edal.position.HorizontalPosition;
 import uk.ac.rdg.resc.edal.util.Array1D;
-import uk.ac.rdg.resc.edal.util.ImmutableArray1D;
+import uk.ac.rdg.resc.edal.util.GISUtils;
 import uk.ac.rdg.resc.edal.util.PlottingDomainParams;
 import uk.ac.rdg.resc.edal.util.TimeUtils;
 import uk.ac.rdg.resc.edal.util.ValuesArray1D;
@@ -89,107 +84,125 @@ public class WaterMLDatasetFactory extends DatasetFactory {
     @Override
     public Dataset createDataset(String id, String location) throws IOException, EdalException {
         try {
+            /*
+             * First we read the GetSiteInfoFile.xml which maps site codes to
+             * physical locations.
+             */
             Map<String, HorizontalPosition> sites = new HashMap<>();
             File wmlSites = new File(location + "/" + "GetSiteInfoFile.xml");
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder builder;
-            builder = factory.newDocumentBuilder();
+            DocumentBuilder builder = factory.newDocumentBuilder();
             Document doc = builder.parse(wmlSites);
             XPathFactory xPathfactory = XPathFactory.newInstance();
             XPath xpath = xPathfactory.newXPath();
-            XPathExpression expr = xpath.compile("timeSeriesResponse/timeSeries");
-            NodeList list = (NodeList) expr.evaluate(doc, XPathConstants.NODESET);
-            for (int i = 0; i < list.getLength(); i++) {
-                Node node = list.item(i);
-                String siteCode = xpath.compile("sourceInfo/siteCode").evaluate(node);
-                Double latitude = (Double) xpath.compile(
-                        "sourceInfo/geoLocation/geogLocation/latitude").evaluate(node,
-                        XPathConstants.NUMBER);
-                Double longitude = (Double) xpath.compile(
-                        "sourceInfo/geoLocation/geogLocation/longitude").evaluate(node,
-                        XPathConstants.NUMBER);
+            /*
+             * Find all elements within timeSeriesResponse/timeSeries - this is
+             * a list of nodes each of which contain an individual site
+             * definition
+             */
+            NodeList sitesList = (NodeList) xpath.compile("timeSeriesResponse/timeSeries")
+                    .evaluate(doc, XPathConstants.NODESET);
+            /*
+             * Precompile some XPath expressions for convenience
+             */
+            XPathExpression siteCodeExpr = xpath.compile("sourceInfo/siteCode");
+            XPathExpression latitudeExpr = xpath
+                    .compile("sourceInfo/geoLocation/geogLocation/latitude");
+            XPathExpression longitudeExpr = xpath
+                    .compile("sourceInfo/geoLocation/geogLocation/longitude");
+            for (int i = 0; i < sitesList.getLength(); i++) {
+                Node site = sitesList.item(i);
+                String siteCode = siteCodeExpr.evaluate(site);
+                Double latitude = (Double) latitudeExpr.evaluate(site, XPathConstants.NUMBER);
+                Double longitude = (Double) longitudeExpr.evaluate(site, XPathConstants.NUMBER);
                 sites.put(siteCode, new HorizontalPosition(longitude, latitude,
                         DefaultGeographicCRS.WGS84));
             }
-            for (Entry<String, HorizontalPosition> entry : sites.entrySet()) {
-                System.out.println(entry.getKey() + " --> " + entry.getValue());
-            }
+            /*
+             * We now have a Map of site code to physical location.
+             * 
+             * Now read the ExportValues file which contains the actual
+             * timeseries
+             */
 
             File wmlData = new File(location + "/" + "ExportValues.xml");
             builder = factory.newDocumentBuilder();
             doc = builder.parse(wmlData);
-            expr = xpath.compile("timeSeriesResponse/timeSeries");
-            list = (NodeList) expr.evaluate(doc, XPathConstants.NODESET);
-            Map<String, Map<String, Array1D<Number>>> values = new HashMap<>();
-            Map<String, TimeAxis> axes = new HashMap<>();
-            Map<String, Parameter> parameters = new HashMap<>();
-            for (int i = 0; i < list.getLength(); i++) {
-                Node node = list.item(i);
-                String varId = xpath.compile("variable/variableCode").evaluate(node);
-                if(varId.equals("cota_d")) {
-                    continue;
-                }
-                String siteCode = xpath.compile("sourceInfo/siteCode").evaluate(node);
-                NodeList list2 = (NodeList) xpath.compile("values/value").evaluate(node,
+            NodeList timeseriesList = (NodeList) xpath.compile("timeSeriesResponse/timeSeries")
+                    .evaluate(doc, XPathConstants.NODESET);
+
+            Collection<PointSeriesFeature> features = new ArrayList<>();
+            XPathExpression varIdExpr = xpath.compile("variable/variableCode");
+            XPathExpression typeExpr = xpath.compile("variable/valueType");
+            XPathExpression valuesExpr = xpath.compile("values/value");
+            XPathExpression unitsExpr = xpath.compile("variable/units");
+            for (int i = 0; i < timeseriesList.getLength(); i++) {
+                Node timeseries = timeseriesList.item(i);
+                String siteCode = siteCodeExpr.evaluate(timeseries);
+                String varId = varIdExpr.evaluate(timeseries);
+                String featureId = siteCode + ":" + varId;
+                NodeList valuesList = (NodeList) valuesExpr.evaluate(timeseries,
                         XPathConstants.NODESET);
                 List<DateTime> axisValues = new ArrayList<>();
-                Array1D<Number> dataValues = new ValuesArray1D(list2.getLength());
-                for (int j = 0; j < list2.getLength(); j++) {
-                    Node n = list2.item(j);
-                    DateTime time = TimeUtils.iso8601ToDateTime(
-                            n.getAttributes().getNamedItem("dateTime").getNodeValue(),
-                            ISOChronology.getInstance());
+                Array1D<Number> dataValues = new ValuesArray1D(valuesList.getLength());
+                for (int j = 0; j < valuesList.getLength(); j++) {
+                    Node valueNode = valuesList.item(j);
+                    DateTime time = TimeUtils.iso8601ToDateTime(valueNode.getAttributes()
+                            .getNamedItem("dateTime").getNodeValue(), ISOChronology.getInstance());
                     axisValues.add(time);
-                    Double value = Double.parseDouble(n.getTextContent());
+                    Double value = Double.parseDouble(valueNode.getTextContent());
                     if (value != null && value == -9999) {
                         value = null;
                     }
                     dataValues.set(value, j);
                 }
-                if (!values.containsKey(siteCode)) {
-                    Map<String, Array1D<Number>> varValues = new HashMap<>();
-                    values.put(siteCode, varValues);
-                }
-                
-                values.get(siteCode).put(varId ,dataValues);
-                System.out.println(dataValues.getShape()[0]);
-
-                if (!axes.containsKey(siteCode)) {
-                    /*
-                     * Assumes that time axes are the same for all variables.
-                     * Need to check this is true...
-                     */
-                    axes.put(siteCode, new TimeAxisImpl("time", axisValues));
-                }
-                /*
-                 * TODO this can be got from the waterML
-                 */
-                parameters.put(varId, new Parameter(varId, varId, varId, "m", "water level"));
+                Map<String, Array1D<Number>> valuesMap = new HashMap<>();
+                valuesMap.put(varId, dataValues);
+                Node evaluate = (Node) unitsExpr.evaluate(timeseries, XPathConstants.NODE);
+                Map<String, Parameter> parameters = new HashMap<>();
+                parameters.put(varId, new Parameter(varId, varId, null, evaluate.getAttributes()
+                        .getNamedItem("unitsCode").getNodeValue(), typeExpr.evaluate(timeseries)));
+                features.add(new PointSeriesFeature(featureId, varId + " at " + siteCode,
+                        "Timeseries feature of " + varId + " at site " + siteCode,
+                        new TimeAxisImpl("Time axis for " + varId, axisValues),
+                        sites.get(siteCode), null, parameters, valuesMap));
             }
-            /*
-             * We now have data for all PSFs
-             */
-            int i=0;
-            Map<String, PointSeriesFeature> features = new HashMap<>();
-            for (String site : values.keySet()) {
-                PointSeriesFeature feature = new PointSeriesFeature(site,
-                        "Timeseries feature at site " + site, "", axes.get(site), sites.get(site), null, parameters,
-                        values.get(site));
-                features.put("feature"+(i++), feature);
-            }
-
             Collection<VariableMetadata> metadata = new ArrayList<>();
-            for(Entry<String, Parameter> entry : parameters.entrySet()){
-                /*
-                 * TOTAL HACK!!!
-                 * 
-                 * Just use a SimpleTemporalDomain
-                 * 
-                 * This works but it's a quick hack, as proof of concept
-                 */
-                metadata.add(new VariableMetadata(entry.getValue(), new SimpleHorizontalDomain(-180, -90, 180, 90), null, axes.get("TP1741")));
+            /*
+             * We can't use Parameter as the key to the bounds object, because
+             * Parameters can be considered equal even if they have different
+             * IDs
+             */
+            Map<Parameter, List<HorizontalDomain>> hDomains = new HashMap<>();
+            Map<Parameter, List<TemporalDomain>> tDomains = new HashMap<>();
+            for (PointSeriesFeature feature : features) {
+                for (String parameterId : feature.getParameterIds()) {
+                    Parameter parameter = feature.getParameter(parameterId);
+                    if (!hDomains.containsKey(parameter)) {
+                        hDomains.put(parameter, new ArrayList<HorizontalDomain>());
+                        tDomains.put(parameter, new ArrayList<TemporalDomain>());
+                    }
+                    HorizontalPosition pos = feature.getHorizontalPosition();
+                    hDomains.get(parameter).add(
+                            new SimpleHorizontalDomain(pos.getX(), pos.getY(), pos.getX(), pos
+                                    .getY(), pos.getCoordinateReferenceSystem()));
+                    tDomains.get(parameter).add(feature.getDomain());
+                }
             }
-            return new WaterMLDataset(id, metadata, features);
+            for (Parameter p : hDomains.keySet()) {
+                metadata.add(new VariableMetadata(p, GISUtils
+                        .getIntersectionOfHorizontalDomains(hDomains.get(p).toArray(
+                                new HorizontalDomain[0])), null, GISUtils
+                        .getIntersectionOfTemporalDomains(tDomains.get(p).toArray(
+                                new TemporalDomain[0]))));
+            }
+            FeatureIndexer featureIndexer = new PRTreeFeatureIndexer();
+            List<FeatureBounds> featureBounds = new ArrayList<>();
+            for (PointSeriesFeature feature : features) {
+                featureBounds.add(FeatureBounds.fromPointSeriesFeature(feature));
+            }
+            featureIndexer.addFeatures(featureBounds);
+            return new WaterMLDataset(id, metadata, featureIndexer, features);
         } catch (ParserConfigurationException | SAXException | XPathExpressionException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
@@ -198,37 +211,13 @@ public class WaterMLDatasetFactory extends DatasetFactory {
     }
 
     class WaterMLDataset extends AbstractPointDataset<PointSeriesFeature> {
-        public WaterMLDataset(String id, Collection<VariableMetadata> vars, final Map<String, PointSeriesFeature> features) {
-            super(id, vars, new FeatureIndexer(){
-
-                @Override
-                public Collection<String> findFeatureIds(BoundingBox horizontalExtent,
-                        Extent<Double> verticalExtent, Extent<DateTime> timeExtent,
-                        Collection<String> variableIds) {
-                    Collection<String> ret = new ArrayList<>();
-                    for(Entry<String, PointSeriesFeature> f : features.entrySet()) {
-                        if(horizontalExtent.contains(f.getValue().getHorizontalPosition())){
-                            ret.add(f.getKey());
-                        }
-                    }
-                    return ret;
-                }
-
-                @Override
-                public Set<String> getAllFeatureIds() {
-                    return features.keySet();
-                }
-
-                @Override
-                public void addFeatures(List<FeatureBounds> features) {
-                    throw new UnsupportedOperationException();
-                }});
-            featureReader = new WaterMLFeatureReader(features);
-            timeAxis = features.get(features.keySet().iterator().next()).getDomain();
-        }
-
-        private TimeAxis timeAxis;
         private WaterMLFeatureReader featureReader;
+
+        public WaterMLDataset(String id, Collection<VariableMetadata> vars,
+                FeatureIndexer featureIndexer, Collection<PointSeriesFeature> features) {
+            super(id, vars, featureIndexer);
+            featureReader = new WaterMLFeatureReader(features);
+        }
 
         @Override
         public Class<? extends DiscreteFeature<?, ?>> getFeatureType(String variableId) {
@@ -248,53 +237,28 @@ public class WaterMLDatasetFactory extends DatasetFactory {
         @Override
         protected PointFeature convertFeature(PointSeriesFeature feature,
                 PlottingDomainParams params) {
-            TimeAxis timeAxis = feature.getDomain();
-            int index = timeAxis.findIndexOf(params.getTargetT());
-            Map<String, Array1D<Number>> values = new HashMap<>();
-            for (String paramId : feature.getParameterIds()) {
-                values.put(paramId, new ImmutableArray1D<>(new Number[] { feature
-                        .getValues(paramId).get(index) }));
-            }
-
-            return new PointFeature(feature.getId(), feature.getName(), feature.getDescription(),
-                    new GeoPosition(feature.getHorizontalPosition(), feature.getVerticalPosition(),
-                            timeAxis.getCoordinateValue(index)), feature.getParameterMap(), values);
+            return convertPointSeriesFeature(feature, params);
         }
 
         @Override
         public DiscreteFeatureReader<PointSeriesFeature> getFeatureReader() {
             return featureReader;
         }
-
-        @Override
-        protected BoundingBox getDatasetBoundingBox() {
-            return BoundingBoxImpl.global();
-        }
-
-        @Override
-        protected Extent<Double> getDatasetVerticalExtent() {
-            return null;
-        }
-
-        @Override
-        protected Extent<DateTime> getDatasetTimeExtent() {
-            return timeAxis.getCoordinateExtent();
-        }
-
     }
 
     private final class WaterMLFeatureReader implements DiscreteFeatureReader<PointSeriesFeature> {
         private Map<String, PointSeriesFeature> features;
 
-        public WaterMLFeatureReader(Map<String, PointSeriesFeature> features) {
-            this.features = features;
+        public WaterMLFeatureReader(Collection<PointSeriesFeature> features) {
+            this.features = new HashMap<>();
+            for (PointSeriesFeature feature : features) {
+                this.features.put(feature.getId(), feature);
+            }
         }
 
         @Override
         public PointSeriesFeature readFeature(String id, Set<String> variableIds)
                 throws DataReadingException {
-            System.out.println("trying to read id: "+id+","+features.get(id));
-            
             return features.get(id);
         }
 
@@ -302,10 +266,7 @@ public class WaterMLDatasetFactory extends DatasetFactory {
         public Collection<PointSeriesFeature> readFeatures(Collection<String> ids,
                 Set<String> variableIds) throws DataReadingException {
             Collection<PointSeriesFeature> ret = new ArrayList<>();
-            for(String id : features.keySet()){
-                System.out.println(id+" is a real id");
-            }
-            for(String id : ids) {
+            for (String id : ids) {
                 ret.add(readFeature(id, variableIds));
             }
             return ret;
