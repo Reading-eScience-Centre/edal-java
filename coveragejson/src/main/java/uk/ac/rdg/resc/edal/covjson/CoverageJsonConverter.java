31d5ec21819acbdbda5209817a532d5588fefadb
Maik Riechert <maik.riechert@arcor.de>
2016-01-12 09:14:33 +0000

    add license headers and @author tags

--- coveragejson/src/main/java/uk/ac/rdg/resc/edal/covjson/CoverageJsonConverter.java
+++ coveragejson/src/main/java/uk/ac/rdg/resc/edal/covjson/CoverageJsonConverter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015 The University of Reading
+ * Copyright (c) 2016 The University of Reading
  * All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without
